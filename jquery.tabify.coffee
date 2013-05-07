d1682c61a0cbadf8fdbaae1613656a43dd90d126
Takeshi Takatsudo <takazudo@gmail.com>
2013-05-08 02:51:50 +0900

    almost coded first version

--- jquery.tabify.coffee
+++ jquery.tabify.coffee
@@ -1,2 +1,248 @@
-do ->
-  console.log 'foo'
+do ($ = jQuery, window = window, document = document) ->
+
+  ns = {}
+  ns.support = {}
+
+  # switch transition support flag by transit plugin.
+  ns.support.transition = do ->
+    return true if $.support?.transition and $.fn.transition?
+    return false
+
+  # ============================================================
+  # utils
+
+  ns.taller = ($el1, $el2) ->
+    height1 = $el1.outerHeight()
+    height2 = $el2.outerHeight()
+    if height1 > height2
+      return $el1
+    else
+      return $el2
+
+  # ============================================================
+  # Tab
+
+  class ns.Tab
+
+    @defaults =
+
+      # selectores related to the container
+      selector_tab: '.tab'
+      selector_contentwrapper: '.tabcontentwrapper' # necessary if you use fade
+      selector_content: '.tabcontentdiv'
+
+      # visual control classNames
+      tab_activeClass: null # 'tab-active'
+      content_activeClass: null # 'tabcontentdiv-active'
+
+      # attr names to detect target
+      attr_target: 'data-tabify-target'
+      attr_id: 'data-tabify-id'
+
+      # update location or not
+      changeHash: false
+
+      # effect
+      useFade: false
+      useTransition: false
+      fadeDuration: 400
+    
+    constructor: (@$el, options = {}) ->
+
+      @options = $.extend {}, ns.Tab.defaults, options
+      @_transitionEnabled = ns.support.transition and @options.useTransition
+      @_eventify()
+
+    _eventify: ->
+
+      @$el.delegate @options.selector_tab, 'click', (e) =>
+        e.preventDefault() unless @options.changeHash is true
+        @switchFromOpener $(e.currentTarget)
+      return this
+
+    switchFromOpener: ($opener) ->
+
+      $lastContentEl = @$lastContentEl or do =>
+        return @$lastContentEl = @$el.find ".#{@options.content_activeClass}"
+      $nextContentEl = @getRelatedContentEl $opener
+
+      return this if $lastContentEl[0] is $nextContentEl[0]
+
+      # if there's already fadeIn progress, fail it.
+      @_lastFadeDefer?.reject()
+      # then force it to hide
+      @disableContentEl $lastContentEl
+
+      if @options.useFade
+
+        # we need to make elements to absolute for fading
+        @makeContentElsToAbsolute()
+
+        # change wrapper's height to taller content
+        $taller = ns.taller $lastContentEl, $nextContentEl
+        @fixWrapperTo $taller
+
+      eventData =
+        clickedTabEl: $opener
+        lastContentEl: $lastContentEl
+        contentEl: $nextContentEl
+
+      @$el.trigger 'tabify.switch', eventData
+      @$el.trigger 'tabify.beforeswitchanimation', eventData
+
+      # save next as last
+      @$lastContentEl = $nextContentEl
+
+      # save defer to fail this when another fade starts
+      @_lastFadeDefer = (@activateContentEl $nextContentEl)
+
+      @_lastFadeDefer.done =>
+        # change wrapper's height again
+        if @options.useFade and ($taller[0] isnt $nextContentEl[0])
+          @fixWrapperTo $nextContentEl
+        @$el.trigger 'tabify.afterswitchanimation', eventData
+
+      # swtich tab
+      @disableActiveTab()
+      @activateTab $opener
+
+      return this
+
+    # wrapper handlers
+    
+    getWrapperEl: ->
+      return @$wrapper or do =>
+        @$wrapper = @$el.find @options.selector_contentwrapper
+        return @$wrapper
+
+    fixWrapperTo: ($contentEl) ->
+      @getWrapperEl().height $contentEl.outerHeight()
+      return this
+
+    # content element handlers
+
+    makeContentElsToAbsolute: ->
+      $contentEls = @$el.find @options.selector_content
+      $contentEls.css
+        position: 'absolute'
+        left:0
+        top:0
+      return this
+
+    activateContentEl: ($contentEl) ->
+
+      defer = $.Deferred (defer) =>
+
+        cls = @options.content_activeClass
+
+        callback = =>
+          $contentEl.addClass cls
+          defer.resolve()
+
+        if @options.useFade
+          d = @options.fadeDuration
+          if @_transitionEnabled
+            $contentEl.show().css('opacity', 0).transition { opacity: 1 }, d, callback
+          else
+            $contentEl.fadeTo d, 1, callback
+        else
+          callback()
+
+      return defer
+
+    disableContentEl: ($contentEl) ->
+
+      handleCls = =>
+        $contentEl.removeClass @options.content_activeClass
+
+      if @options.useFade
+        if @_transitionEnabled
+          $contentEl.stop().css('opacity', 0).hide()
+        else
+          $contentEl.stop().fadeTo(0, 0).hide()
+        handleCls()
+      else
+        handleCls()
+
+      return this
+
+    # tab element handlers
+    
+    getLastTab: ->
+
+      return @$lastTab or do =>
+        cls = @options.tab_activeClass
+        $tabs = (@$el.find @options.selector_tab)
+        @$lastTab = $tabs.filter (i, el) ->
+          return $(el).hasClass cls
+        return @$lastTab
+
+    activateTab: ($opener) ->
+
+      $opener.addClass @options.tab_activeClass
+      @$lastTab = $opener
+      return this
+
+    disableActiveTab: ->
+
+      cls = @options.tab_activeClass
+      @getLastTab().removeClass cls
+      return this
+
+    # helpers
+
+    getRelatedContentEl: ($tab) ->
+
+      $contentEls = @$el.find @options.selector_content
+      targetData = $tab.attr @options.attr_target
+
+      # if data attr was requested, find el from data attr
+      if targetData
+        $filtered = $contentEls.filter (i, el) =>
+          if ($(el).attr @options.attr_id) is targetData
+            return true
+          else
+            return false
+        if $filtered.length is 1
+          return $filtered
+
+      # else, use href as id selector
+      href = $tab.attr 'href'
+      return @$el.find href
+
+    # control methods
+    
+    switchById: (id) ->
+      if @options.changeHash
+        location.href = "##{id}"
+      $opener = (@$el.find @options.selector_tab).filter (i, el) ->
+        return ($(el).attr 'href') is "##{id}"
+      if $opener.length
+        @switchFromOpener $opener
+      return this
+
+    switchByTabId: (id) ->
+      $opener = (@$el.find @options.selector_tab).filter (i, el) =>
+        return ($(el).attr @options.attr_target) is id
+      if $opener.length
+        @switchFromOpener $opener
+      return this
+
+
+  # ============================================================
+  # jQuery bridges
+
+  $.fn.tabify = (options) ->
+
+    return @each ->
+
+      $el = $(@)
+      tab = new ns.Tab $el, options
+      $el.data 'tabify', tab
+      return
+
+  # ============================================================
+  # globalify
+
+  $.Tabify = ns
+
--- jquery.tabify.js
+++ jquery.tabify.js
@@ -1,12 +1,261 @@
 /*! jQuery.tabify (https://github.com/Takazudo/jQuery.tabify)
- * lastupdate: 2013-05-07
+ * lastupdate: 2013-05-08
  * version: 0.0.0
  * author: 'Takazudo' Takeshi Takatsudo <takazudo@gmail.com>
  * License: MIT */
 (function() {
 
-  (function() {
-    return console.log('foo');
-  })();
+  (function($, window, document) {
+    var ns;
+    ns = {};
+    ns.support = {};
+    ns.support.transition = (function() {
+      var _ref;
+      if (((_ref = $.support) != null ? _ref.transition : void 0) && ($.fn.transition != null)) {
+        return true;
+      }
+      return false;
+    })();
+    ns.taller = function($el1, $el2) {
+      var height1, height2;
+      height1 = $el1.outerHeight();
+      height2 = $el2.outerHeight();
+      if (height1 > height2) {
+        return $el1;
+      } else {
+        return $el2;
+      }
+    };
+    ns.Tab = (function() {
+
+      Tab.defaults = {
+        selector_tab: '.tab',
+        selector_contentwrapper: '.tabcontentwrapper',
+        selector_content: '.tabcontentdiv',
+        tab_activeClass: null,
+        content_activeClass: null,
+        attr_target: 'data-tabify-target',
+        attr_id: 'data-tabify-id',
+        changeHash: false,
+        useFade: false,
+        useTransition: false,
+        fadeDuration: 400
+      };
+
+      function Tab($el, options) {
+        this.$el = $el;
+        if (options == null) {
+          options = {};
+        }
+        this.options = $.extend({}, ns.Tab.defaults, options);
+        this._transitionEnabled = ns.support.transition && this.options.useTransition;
+        this._eventify();
+      }
+
+      Tab.prototype._eventify = function() {
+        var _this = this;
+        this.$el.delegate(this.options.selector_tab, 'click', function(e) {
+          if (_this.options.changeHash !== true) {
+            e.preventDefault();
+          }
+          return _this.switchFromOpener($(e.currentTarget));
+        });
+        return this;
+      };
+
+      Tab.prototype.switchFromOpener = function($opener) {
+        var $lastContentEl, $nextContentEl, $taller, eventData, _ref,
+          _this = this;
+        $lastContentEl = this.$lastContentEl || (function() {
+          return _this.$lastContentEl = _this.$el.find("." + _this.options.content_activeClass);
+        })();
+        $nextContentEl = this.getRelatedContentEl($opener);
+        if ($lastContentEl[0] === $nextContentEl[0]) {
+          return this;
+        }
+        if ((_ref = this._lastFadeDefer) != null) {
+          _ref.reject();
+        }
+        this.disableContentEl($lastContentEl);
+        if (this.options.useFade) {
+          this.makeContentElsToAbsolute();
+          $taller = ns.taller($lastContentEl, $nextContentEl);
+          this.fixWrapperTo($taller);
+        }
+        eventData = {
+          clickedTabEl: $opener,
+          lastContentEl: $lastContentEl,
+          contentEl: $nextContentEl
+        };
+        this.$el.trigger('tabify.switch', eventData);
+        this.$el.trigger('tabify.beforeswitchanimation', eventData);
+        this.$lastContentEl = $nextContentEl;
+        this._lastFadeDefer = this.activateContentEl($nextContentEl);
+        this._lastFadeDefer.done(function() {
+          if (_this.options.useFade && ($taller[0] !== $nextContentEl[0])) {
+            _this.fixWrapperTo($nextContentEl);
+          }
+          return _this.$el.trigger('tabify.afterswitchanimation', eventData);
+        });
+        this.disableActiveTab();
+        this.activateTab($opener);
+        return this;
+      };
+
+      Tab.prototype.getWrapperEl = function() {
+        var _this = this;
+        return this.$wrapper || (function() {
+          _this.$wrapper = _this.$el.find(_this.options.selector_contentwrapper);
+          return _this.$wrapper;
+        })();
+      };
+
+      Tab.prototype.fixWrapperTo = function($contentEl) {
+        this.getWrapperEl().height($contentEl.outerHeight());
+        return this;
+      };
+
+      Tab.prototype.makeContentElsToAbsolute = function() {
+        var $contentEls;
+        $contentEls = this.$el.find(this.options.selector_content);
+        $contentEls.css({
+          position: 'absolute',
+          left: 0,
+          top: 0
+        });
+        return this;
+      };
+
+      Tab.prototype.activateContentEl = function($contentEl) {
+        var defer,
+          _this = this;
+        defer = $.Deferred(function(defer) {
+          var callback, cls, d;
+          cls = _this.options.content_activeClass;
+          callback = function() {
+            $contentEl.addClass(cls);
+            return defer.resolve();
+          };
+          if (_this.options.useFade) {
+            d = _this.options.fadeDuration;
+            if (_this._transitionEnabled) {
+              return $contentEl.show().css('opacity', 0).transition({
+                opacity: 1
+              }, d, callback);
+            } else {
+              return $contentEl.fadeTo(d, 1, callback);
+            }
+          } else {
+            return callback();
+          }
+        });
+        return defer;
+      };
+
+      Tab.prototype.disableContentEl = function($contentEl) {
+        var handleCls,
+          _this = this;
+        handleCls = function() {
+          return $contentEl.removeClass(_this.options.content_activeClass);
+        };
+        if (this.options.useFade) {
+          if (this._transitionEnabled) {
+            $contentEl.stop().css('opacity', 0).hide();
+          } else {
+            $contentEl.stop().fadeTo(0, 0).hide();
+          }
+          handleCls();
+        } else {
+          handleCls();
+        }
+        return this;
+      };
+
+      Tab.prototype.getLastTab = function() {
+        var _this = this;
+        return this.$lastTab || (function() {
+          var $tabs, cls;
+          cls = _this.options.tab_activeClass;
+          $tabs = _this.$el.find(_this.options.selector_tab);
+          _this.$lastTab = $tabs.filter(function(i, el) {
+            return $(el).hasClass(cls);
+          });
+          return _this.$lastTab;
+        })();
+      };
+
+      Tab.prototype.activateTab = function($opener) {
+        $opener.addClass(this.options.tab_activeClass);
+        this.$lastTab = $opener;
+        return this;
+      };
+
+      Tab.prototype.disableActiveTab = function() {
+        var cls;
+        cls = this.options.tab_activeClass;
+        this.getLastTab().removeClass(cls);
+        return this;
+      };
+
+      Tab.prototype.getRelatedContentEl = function($tab) {
+        var $contentEls, $filtered, href, targetData,
+          _this = this;
+        $contentEls = this.$el.find(this.options.selector_content);
+        targetData = $tab.attr(this.options.attr_target);
+        if (targetData) {
+          $filtered = $contentEls.filter(function(i, el) {
+            if (($(el).attr(_this.options.attr_id)) === targetData) {
+              return true;
+            } else {
+              return false;
+            }
+          });
+          if ($filtered.length === 1) {
+            return $filtered;
+          }
+        }
+        href = $tab.attr('href');
+        return this.$el.find(href);
+      };
+
+      Tab.prototype.switchById = function(id) {
+        var $opener;
+        if (this.options.changeHash) {
+          location.href = "#" + id;
+        }
+        $opener = (this.$el.find(this.options.selector_tab)).filter(function(i, el) {
+          return ($(el).attr('href')) === ("#" + id);
+        });
+        if ($opener.length) {
+          this.switchFromOpener($opener);
+        }
+        return this;
+      };
+
+      Tab.prototype.switchByTabId = function(id) {
+        var $opener,
+          _this = this;
+        $opener = (this.$el.find(this.options.selector_tab)).filter(function(i, el) {
+          return ($(el).attr(_this.options.attr_target)) === id;
+        });
+        if ($opener.length) {
+          this.switchFromOpener($opener);
+        }
+        return this;
+      };
+
+      return Tab;
+
+    })();
+    $.fn.tabify = function(options) {
+      return this.each(function() {
+        var $el, tab;
+        $el = $(this);
+        tab = new ns.Tab($el, options);
+        $el.data('tabify', tab);
+      });
+    };
+    return $.Tabify = ns;
+  })(jQuery, window, document);
 
 }).call(this);
