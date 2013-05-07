61d7286fe9bd66467575db433348b04411be13c8
Takeshi Takatsudo <takazudo@gmail.com>
2013-05-08 03:01:30 +0900

    bumped version number to v1.0.0

--- jquery.tabify.js
+++ jquery.tabify.js
@@ -1,6 +1,6 @@
 /*! jQuery.tabify (https://github.com/Takazudo/jQuery.tabify)
  * lastupdate: 2013-05-08
- * version: 0.0.0
+ * version: 1.0.0
  * author: 'Takazudo' Takeshi Takatsudo <takazudo@gmail.com>
  * License: MIT */
 (function() {
